03564557ab1c41991f964d636cb20ace4b2ebd7a
Bikram Karki <bkarki@memphis.edu>
2019-01-31 07:46:01 +0000

    Add About me page and software Review page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,11 @@
 class PagesController < ApplicationController
+
+    def bkarki
+        # render 'bkarki.html.erb'
+    end    
+
+    def reviewsoftware
+        # render 'reviewsoftware.html.erb'
+    end    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+ 
+  get 'bkarki', to: 'pages#bkarki', as: 'bkarki' 
+  get 'reviewsoftware', to: 'pages#reviewsoftware', as: 'reviewsoftware' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
